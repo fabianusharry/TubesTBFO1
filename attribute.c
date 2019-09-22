10bdaf0f8f05c4e05bca0e9db01117cc0f6c21ad
fabianusharry <fabianusharry@yahoo.com>
2019-09-22 20:14:14 +0700

    Realisasi header attribute

--- attribute.c
+++ attribute.c
@@ -26,36 +26,82 @@ typedef struct {
 #define Fun(A)  (A).Fun
 
 /* *** KONSTRUKTOR *** */
-boolean IsAtrValid(Atr i);
+boolean IsAtrValid(Atr i){
+    return((i >= 0) && (i<=15));
+}
 /* Mengecek apakah i >=0 dan <=15 */
 
-void PesanError();
+void PesanError(){
+    printf("Aksi Tidak Valid\n");
+}
 /* Mengoutput pesan error yaitu: Aksi Tidak Valid */
 
-void TulisAttribute(Attribute A);
+void TulisAttribute(Attribute A){
+    printf("Hygiene = %d\n", Hygiene(A));
+    printf("Energy = %d\n", Energy(A));
+    printf("Fun = %d\n", Fun(A));
+}
 /* Menuliskan Attribute karakter dengan format:
 Hygiene = <Total_Hygiene>
 Energy = <Total_Energy>
 Fun = <Total_Fun> */
 
-Attribute NewChar();
+Attribute NewChar(){
+    Attribute A;
+    Hygiene(A) = 0;
+    Energy(A) = 10;
+    Fun(A) = 0;
+    return A; 
+}
 /* Mengassign  Kondisi awal pemain dengan atribut Hygiene bernilai 0, Energy
 bernilai 10, dan Fun bernilai 0. Permainan dinyatakan selesai jika semua atribut bernilai 0 atau
 semua atribut bernilai 15.*/
 
 /* Operator Menambahkan/ mengurangi attribut
    Attribute harus valid, bila tidak gunakan pesanerror*/
-void ChangesHygiene(Atr i,Attribute *A);
+void AddHygiene(Atr i,Attribute *A){
+    if(IsAtrValid(Hygiene(A) + i)){
+        Hygiene(A) += i
+    }
+    else{
+        PesanError();
+    }
+}
 /* Menambahkan Hygiene sebesar i*/
 
-void ChangeEnergy(Atr i,Attribute *A);
+void AddEnergy(Atr i,Attribute *A){
+    if(IsAtrValid(Energy(A) + i)){
+        Energy(A) += i
+    }
+    else{
+        PesanError();
+    }
+}
 /* Menambahkan Energy sebesar i*/
 
-void ChangeFun(Atr i,Attribute *A);
+void AddFun(Atr i,Attribute *A){
+    if(IsAtrValid(Hygiene(A) + i)){
+        Hygiene(A) += i
+    }
+    else{
+        PesanError();
+    }
+}
 /* Menambahkan Fun sebesar i*/
 
+void RedHygiene(Atr i,Attribute *A);
+/* Mengurangi Fun sebanyak i*/
+
+void RedEnergy(Atr i,Attribute *A);
+/* Mengurangi Fun sebanyak i*/
+
+void RedFun(Atr i,Attribute *A);
+/* Mengurangi Fun sebanyak i*/
+
 /* Operator End Game */
-boolean IsGameOver(Attribute A);
+boolean IsGameOver(Attribute A){
+    if((Hygiene(A) == 15 && Energy(A) == 15 && Fun(A) == 15) || (Hygiene(A) == 0 && Energy(A) == 0 && Fun(A) == 0))
+}
 /* Mereturn true bila semua atribut 15 atau 0 */
 
 #endif
