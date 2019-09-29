2197237f60913bcddaa34366cc2aedf93f128081
Mario Gunawan <margunwa123@yahoo.com>
2019-09-30 04:42:06 +0700

    SEDHAP
    maksudnya DFA di csv apa anjir

--- Tubes.py
+++ Tubes.py
@@ -1,5 +1,8 @@
 
 ######################################## KAMUS #######################################
+import csv
+csvData = [["Nama Aksi", "Hygiene","Energy","Fun"]]
+#untuk memasukan dan mengedit file
 
 ############ VALIDATOR #################
 def isInputValid(a,b,c) :
@@ -20,6 +23,41 @@ def check():        ### FINAL STATE ###
         kalah = True
 
 #Fungsi untuk mengecek apakah pemain sudah dalam kondisi menang atau kalah
+
+def AddAksi(Action):
+    global Hygiene
+    global Energy
+    global Fun
+    global csvData
+    row = [Action, str(Hygiene), str(Energy), str(Fun)]
+    csvData.append(row)
+
+#Fungsi untuk memasukan perintah kedalam list
+
+def InputFile():
+    global nama
+    global fileName
+    global cekFile
+    isFile = input("Apakah kamu mau memasukan data game ini kedalam file? (Yes/No) : ")
+    while(isFile != "No" and isFile != "Yes"):
+        isFile = input("Mohon ulangi pilihan anda (Yes/No) : ")
+    
+    nama = input("Masukkan Nama SIMS : ")
+    if (isFile == "Yes") :
+        fileName = nama + ".csv"
+        #fileName = input("Masukan nama file (gunakan .csv di akhir) : ")
+        print("Silakan memainkan game, file akan disave di " + fileName)
+        cekFile = True
+    
+def TulisKeFile(): # $ Fungsi untuk menuliskan aksi dan perubahan attribute kedalam file
+    global fileName
+    global csvData
+    with open(fileName, 'w') as writeFile:
+        writer = csv.writer(writeFile)
+        writer.writerows(csvData)
+    writeFile.close()
+
+#Fungsi untuk memasukan sesuatu kedalam file
 
 ############ VALUATOR/MODIFIER ###############
 
@@ -54,8 +92,10 @@ def printattribute():
 #Prosedur untuk mencetak atribut saat ini di layat
 
 def pesanerror():
+    global checkperintah
     print()
     print("Aksi tidak valid!")
+    checkperintah = False
     printattribute()
 
 #Prosedur untuk mengganti attribute secara compact
@@ -74,6 +114,8 @@ def ChangeAttribute(H,E,F):
 #Prosedur untuk mencetak apabila atribut melebihi batas nilai minimum atau maksimum
 
 def deskripsiaksi():
+    global perintah2
+    perintah2 = ""
     print()
     print("[< AKSI YANG BISA DILAKUKAN >]")
     print("1. Tidur")
@@ -93,55 +135,55 @@ def deskripsiaksi():
 #Prosedur mencetak list aksi yang bisa dilakukan
 
 def deskripsitidur():
-    global perintah
+    global perintah2
     print()
     print("Mau tidur apa nih?")
     print("1. Tidur Siang")
     print("2. Tidur Malam")
-    perintah = input("> ")
+    perintah2 = input("> ")
 
 #Prosedur untuk mencetak opsi tidur yang bisa dilakukan dan menerima input dari pemain
 
 def deskripsimakan():
-    global perintah
+    global perintah2
     print()
     print("Mau makan apa nih?")
     print("[< MENU MAKANAN >]")
     print("1. Hamburger")
     print("2. Pizza")
     print("3. Steak and Beans")
-    perintah = input("> ")
+    perintah2 = input("> ")
 
 #Prosedur untuk mencetak opsi makanan apa yang bisa dimakan dan menerima input dari pemain
 
 def deskripsiminum():
-    global perintah
+    global perintah2
     print()
     print("Mau minum apa nih?")
     print("[< MENU MINUMAN >]")
     print("1. Air")
     print("2. Kopi")
     print("3. Jus")
-    perintah = input("> ")
+    perintah2 = input("> ")
 
 #Prosedur untuk mencetak opsi buang air pemain
 
 def deskripsibuangair():
-    global perintah
+    global perintah2
     print()
     print("Mau Buang air kecil atau buang air besar??")
-    print("1. Kencing")
-    print("2. Berak")
-    perintah = input("> ")
+    print("1. Kecil")
+    print("2. Besar")
+    perintah2 = input("> ")
 
 def deskripsimembaca():
-    global perintah
+    global perintah2
     print()
     print("Mau baca apa nih?")
     print("[< LIST BACAAN >]")
     print("1. Koran")
     print("2. Novel")
-    perintah = input("> ")
+    perintah2 = input("> ")
 
 ############ START STATE #################
 def startgame() : 
@@ -152,26 +194,49 @@ def startgame() :
     global kalah
     global perintah
     global nama
-
-    print("[[<<   THE SIMS JADIJADIAN   >>]]")
-    nama = input("Masukkan Nama SIMS : ")
-    print("Halo " + nama + "! Selamat Datang di Permainan SIMS, dan Selamat Pagi :) Berikut Kondisi Atributmu : ")
+    
+    
+    print("[[<<!!!!!!!!!!!!!!!!!!!!!!!!!!!   THE SIMS JADIJADIAN   !!!!!!!!!!!!!!!!!!!!!!!!!!!>>]]")
+    print()
+    print("        `-:/+++++++++/:-.`                                                                      ")
+    print("     `:++++++++++++++++++++:                -:::::::                                            ")
+    print("    -+oooooooooo++ooooooo+:` `........      +ooooooo-     .......`        `.-:///++///:-.`      ")
+    print("   -oooooooo+-.`   `.-/o/`   /oooooooo`    .oooooooo+    .ooooooo+     `:+oooooooooooooooo+:    ")
+    print("   +oooooooo:                /oooooooo`    +ooooooooo-   +oooooooo.   :ooooooooooooooooooo:`    ")
+    print("   +sssssssso/-`             /ssssssss.   .ssssssssss+  -sssssssss+  /sssssssso/:::://oo/`      ")
+    print("   .osssssssssssoo+/-.`      /ssssssss.   :sssssssssss. ossssssssss. ossssssso`        `        ")
+    print("    `+ssssssssssssssssss+:`  +ssssssss.   osssssssssss+:sssssssssss+ osssssssss+/:.`            ")
+    print("      `-/oyyyyyyyyyyyyyyyyy/ :yyyyyyyy.  -yyyyyyyyyyyyyyyyyyyyyyyyyy.`oyyyyyyyyyyyyyyo+-`       ")
+    print("          ///:/osyyyyyyyyyyyo +yyyyyyy.  +yyyyyyyyyyyyyyyyyyyyyyyyyy:  -+yyyyyyyyyyyyyyyy+`     ")
+    print("                 `.+yyyyyyyyy-.yyyyyyy. `yyyyyyyyyyyyyyyyyyyyyyyyyyyy     .-/+osyyyyyyyyyys`    ")
+    print("                    yhhhhhhhh-.hhhhhhh. /hhhhhhhsyhhhhhhhhhhyshhhhhhh:          `-ohhhhhhhh-    ")
+    print("     `+hy+:.`    .:shhhhhhhhy`/hhhhhhh. yhhhhhhh//hhhhhhhhhh/+hhhhhhhs           .+hhhhhhhh-    ")
+    print("   `/hhhhhhhhhhhhhhhhhhhhhhy.-hhhhhhhh.:hhhhhhhh..hhhhhhhhhh..hhhhhhhhohhhhhhhhhhhhhhhhhhho     ")
+    print("  `ohhhhhhhhhhhhhhhhhhhhhh+` ohhhhhhhh.shhhhhhhy  shhhhhhhho  yhhhhhhhhhhhhhhhhhhhhhhhhhy/      ")
+    print("     -/oyhhhhhhhhhhhhys+-    ohhhhhhhh:hhhhhhhh/  -hhhhhhhh.  ohhhhhhhhooyhhhhhhhhhhys+-      ")
+    print()
+    print("Halo! Selamat Datang di Permainan SIMS, dan Selamat Pagi :)")
     Hygiene = 0
     Energy = 10
     Fun = 0
     menang = False
     kalah = False
-    printattribute()
-    print()
+    
+    print("-------------------------------------------------------------------")
     print("Silahkan lakukan apapun yang kamu mau! Tapi ingat, tujuan kamu menang! Yaitu kalo kamu berhasil membuat semua atribut menjadi 15")
     print("Ingat!! Semua hal pasti ada konsekuensinya! Kamu juga bisa kalah kalo atribut kamu 0 semua!")
     print("Selamat Bermain!!")
-
+    print("-------------------------------------------------------------------")
+    InputFile()
+    print("-------------------------------------------------------------------")
+    print("Selamat datang "+nama+ " ke dalam permainan, berikut adalah statusmu : ")
+    printattribute()
     deskripsiaksi()
     perintah = input("> ")
 
 ############ END STATE #################
 def EndGame():
+    global cekFile
     global nama
     if(menang):
         print("ALHAMDULILLAH IDUP MAKMUR EUY MENANGGGG IDUP AMAN TENTRAM ADIL MAKMUR")
@@ -180,58 +245,68 @@ def EndGame():
     elif(kalah):
         print("PEMAIN KAMU MATI WOI!! Belom beruntung :((((")
     
+    if(cekFile):
+        print("Jangan lupa, file akan disave ke : " + fileName)
+        TulisKeFile()
+
     print("Terima kasih sudah bermain The Sims Jadijadian ini hehe!! :)")
+    terakhir = input("(Tekan enter)")
+    
 
 def Cheat():
     global menang
     menang = True
+
 # Fungsi digunakan saat game akan dimulai untuk mempersiapkan semua variabel sesuai dengan state awal game
 
 ################################### ALGORITMA MAIN PROGRAM #################################
-
 startgame()
 while True:
-
+    checkperintah = True
     if((perintah == "Tidur")):
         deskripsitidur()
 
-        if(perintah == "Siang"):
+        if(perintah2 == "Siang"):
             ChangeAttribute(0,10,0)
 
-        elif(perintah == "Malam"):
+        elif(perintah2 == "Malam"):
             ChangeAttribute(0,15,0)
+        else : pesanerror()
 
     elif(perintah == "Makan"):
         deskripsimakan()
-        if(perintah == "Hamburger"):
+        if(perintah2 == "Hamburger"):
             ChangeAttribute(0,5,0)
 
-        elif(perintah == "Pizza"):
+        elif(perintah2 == "Pizza"):
             ChangeAttribute(0,10,0)
         
-        elif(perintah == "Steak and Beans"):
+        elif(perintah2 == "Steak and Beans"):
             ChangeAttribute(0,15,0)
+        else : pesanerror()
 
     elif(perintah == "Minum"):
         deskripsiminum()
 
-        if(perintah == "Air"):
+        if(perintah2 == "Air"):
             ChangeAttribute(-5,0,0)
 
-        elif(perintah == "Kopi"):
+        elif(perintah2 == "Kopi"):
             ChangeAttribute(-10,5,0)
         
-        elif(perintah == "Jus"):
+        elif(perintah2 == "Jus"):
             ChangeAttribute(-5,10,0)
+        else : pesanerror()
         
     elif(perintah == "Buang Air"):
         deskripsibuangair()
 
-        if(perintah == "Kencing"):
+        if(perintah2 == "Kecil"):
             ChangeAttribute(5,0,0)
 
-        elif(perintah == "Berak"):
+        elif(perintah2 == "Besar"):
             ChangeAttribute(10,-5,0)
+        else : pesanerror()
     
     elif(perintah == "Bersosialisasi ke Kafe"):
         ChangeAttribute(-5,-10,15)
@@ -254,24 +329,29 @@ while True:
     elif((perintah == "Membaca")):
         deskripsimembaca()
 
-        if(perintah == "Koran"):
+        if(perintah2 == "Koran"):
             ChangeAttribute(0,-5,5)
 
-        elif(perintah == "Novel"):
+        elif(perintah2 == "Novel"):
             ChangeAttribute(0,-5,10)
 
-    elif(perintah == "Cek Attribute"):
+        else : pesanerror()
+
+    elif(perintah2 == "Cek Attribute"):
         printattribute()
     
-    elif(perintah == "HarryMario"):
+    elif(perintah2 == "HarryMario"):
         Cheat()
     
-    else: print("Aksi tidak terdaftar, hayo dibaca lagi!")
+    else: 
+        print("Aksi tidak terdaftar, hayo dibaca lagi!")
+        checkperintah = False
     check()
+    if(checkperintah):
+        AddAksi(perintah+" "+perintah2)
     if(menang or kalah) : break
     deskripsiaksi()
     perintah = input("Masukkan aksi : ")
 
 EndGame()
-
 #################### E N D   O F   P R O G R A M ######################
